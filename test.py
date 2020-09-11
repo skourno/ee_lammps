97d50eee6fa95bbc056864ab73cb7fe40d0a7a7f
Spiros Kournopoulos <sk1916@ce-amparo05.ce.ic.ac.uk>
2020-09-11 11:05:41 +0100

    some formatting

--- test.py
+++ test.py
@@ -4,31 +4,6 @@
 
 # Written by Srikanth Ravipati (rsri131@gmail.com)
 
-# Not used in a production run; only for debugging
-def find_fractional_charge(q_atoms,type_atoms,idc,natoms):
-    q_Na =  1.0
-    id_atom_Na = -1
-    q_Cl = -1.0
-    id_atom_Cl = -1
-    for i_atom in range(natoms):
-       if (type_atoms[i_atom] == 3 or type_atoms[i_atom] == 4):
-          if (abs(q[i_atom]) < 1.0):
-             if (type_atoms[i_atom] == 3):
-                q_Na = q[i_atom]
-                id_atom_Na = idc[i_atom]
-             else:
-                q_Cl = q[i_atom]
-                id_atom_Cl = idc[i_atom]
-    return id_atom_Na, q_Na, id_atom_Cl, q_Cl
-### To debug at any time
-#####x = lmp.extract_atom("x",3)
-#q = lmp.extract_atom("q",2)
-#t = lmp.extract_atom("type",0)
-#idx = lmp.extract_atom("id",0)
-#data = find_fractional_charge(q,t,idx,natoms)
-
-
-
 import numpy as np
 from mpi4py import MPI
 from Hist import Histogram
@@ -49,13 +24,13 @@ ee_hist_WL = open("ee_hist_WL.dat","w")
 
 
 # Needs to be passed as arguments
-iseed = 623469
+iseed         = 623469
 n_atoms_water = 6402
-i_atom_Na_i = 6403
-i_atom_Na_f = 6460
-i_atom_Cl_i = 6461
-i_atom_Cl_f = 6518
-natoms = 6518
+i_atom_Na_i   = 6403
+i_atom_Na_f   = 6460
+i_atom_Cl_i   = 6461
+i_atom_Cl_f   = 6518
+natoms        = 6518
 idx_test_Na = 6460
 idx_test_Cl = 6518
 
