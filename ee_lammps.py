89599c7e5245e5bece13bd81c12f3496e0a8f575
Spiros Kournopoulos <sk1916@ce-amparo05.ce.ic.ac.uk>
2020-09-13 18:52:50 +0100

    Transition matrix sampling along with the WL sampling

---
+++ ee_lammps.py
@@ -0,0 +1,219 @@
+#-----------------------------------------------------------------------
+#                              ee_lammps.py
+#-----------------------------------------------------------------------
+#
+# A program to perform expanded ensemble simulations of electrolyte
+# solutions. Lammps is used to simulate and explore the individual
+# sub-ensembles, while a biased MC boltzman criterion is used to
+# attempt subensemble changes.
+#
+#-----------------------------------------------------------------------
+#
+#  Written by:
+#
+#  version 0.0 - Srikanth Ravipati  (rsri131@gmail.com)
+#  version 1.0 - Spiros Kournopulos (skourno@gmail.com)
+#
+#  github : https://github.com/skourno/ee_lammps.git
+#
+#-----------------------------------------------------------------------
+
+import numpy as np
+
+from mpi4py   import MPI
+from Hist     import Histogram
+from WL       import WL_histogram
+from tmmc     import TMMC_histogram
+from lammps   import lammps
+from init_sim import Initialize_tip4p_with_Ions_simulation
+
+comm = MPI.COMM_WORLD
+
+# General EE histogram info
+charge_min  = 0.0
+charge_max  = 1.0
+desired_inc = 0.1
+EEHist      = Histogram(charge_min, charge_max, desired_inc)
+
+# WL info
+lnf_initial = 1.0    # initial lnf
+lnf_scaler  = 0.5    
+ratio_crit  = 0.8    
+lnf_crit    = 1.e-4  
+WLHist      = WL_histogram(EEHist,lnf_initial,lnf_scaler,ratio_crit,lnf_crit)
+
+# TMMC Initialization
+TMHist      = TMMC_histogram(EEHist)
+
+WL_FILE_out = open("dos_WL.dat","w") # File to write DOS from WL
+TM_FILE_out = open("dos_TM.dat","w") # File to write DOS from TMMC
+
+# Needs to be passed as arguments through an input file (TODO)
+iseed         = 623469
+n_atoms_water = 6402
+i_atom_Na_i   = 6403
+i_atom_Na_f   = 6460
+i_atom_Cl_i   = 6461
+i_atom_Cl_f   = 6518
+natoms        = 6518
+NIonsPairs    = 58
+time_equil    = 10 # length of equilibration run in fs (no sub-ens change attempts during this time)
+time_sub_sim  = 20 # each sube-ens is simulated for this many fs between sub-ens changee attempts
+Temp          = 298.0
+DataFileName  = "tip4p05.data"
+
+
+wts_init      = np.zeros(WLHist.NSubs,np.double)
+wts_init[ 0]  =  0.000
+wts_init[ 1]  = -1.969
+wts_init[ 2]  = -10.781
+wts_init[ 3]  = -23.906
+wts_init[ 4]  = -43.688
+wts_init[ 5]  = -70.219
+wts_init[ 6]  = -105.344
+wts_init[ 7]  = -146.063
+wts_init[ 8]  = -194.531
+wts_init[ 9]  = -248.875
+wts_init[10]  = -311.344
+
+WLHist.wts    = wts_init # give an initial estimate of the weights
+
+
+lmp = Initialize_tip4p_with_Ions_simulation(DataFileName, Temp, iseed)
+
+# run an initial equilibration run
+lmp.command("variable time_equil string -1")
+flag = lmp.set_variable("time_equil",time_equil)
+lmp.command("run ${time_equil}")
+
+# make auxiliary ion groups
+lmp.command("group na type 3")
+lmp.command("group cl type 4")
+
+# Initializing auxiliary variables
+lmp.command("variable idx_test_Na  string -1")
+lmp.command("variable idx_test_Cl  string -1")
+lmp.command("variable q_test_Na    string  0")
+lmp.command("variable q_test_Cl    string  0")
+lmp.command("variable time_sub_sim string -1")
+
+# Fixing test ions/atom numbers
+idx_test_Na   = 6460
+idx_test_Cl   = 6518
+flag          = lmp.set_variable("idx_test_Na" ,idx_test_Na)
+flag          = lmp.set_variable("idx_test_Cl" ,idx_test_Cl)
+flag          = lmp.set_variable("time_sub_sim",time_sub_sim)
+
+
+Beta          = 1.0 / (Temp * 8.314 / 4184.0)
+delta_q_test  = EEHist.width_bin
+q_current     = 1.0
+acceptTrans   = False
+
+# initialize the following auxiliary variables
+idx_test_dir = 0 # index that decides the direction
+idx_shift_Na = 0 # index to help with choosing a new Na test particle
+idx_shift_Cl = 0 # index to help with choosing a new Cl test particle
+
+
+for i_loop in range(100000):
+  if (np.mod(i_loop,10) == 0 and comm.Get_rank() == 0):
+    tag = "Simulation time: %8d fs" %(time_equil + i_loop*time_sub_sim)
+    WLHist.write(tag, WL_FILE_out)
+
+    TMHist.update_TMMC_weights()     # compute the current estimate of TMMC for the weights
+    TMHist.write(tag, TM_FILE_out) 
+  
+  comm.Barrier()
+  lmp.command("run ${time_sub_sim}")
+
+  e_old        = lmp.extract_compute("thermo_pe",0,0)
+  old_idx_hist = EEHist.idx_of(q_current)
+
+  # If all ion pairs have full fractional charges, 
+  # randomly select ions to be the test pair
+  if (old_idx_hist == EEHist.NBins-1 and comm.Get_rank() == 0):
+    idx_shift_Na = np.random.randint(0,NIonsPairs)
+    idx_test_Na  = i_atom_Na_i + idx_shift_Na 
+    idx_shift_Cl = np.random.randint(0,NIonsPairs)
+    idx_test_Cl  = i_atom_Cl_i + idx_shift_Cl
+
+  idx_test_Na = comm.bcast(idx_test_Na,root=0)
+  idx_test_Cl = comm.bcast(idx_test_Cl,root=0)
+  flag = lmp.set_variable("idx_test_Na",idx_test_Na)
+  flag = lmp.set_variable("idx_test_Cl",idx_test_Cl)
+  
+  # Processor that has rank zero decides which direction to move
+  # and broadcasts to everybody else
+  if (comm.Get_rank() == 0):
+    rand_num = np.random.rand()
+    if (rand_num < 0.5):
+      idx_test_dir = +1
+    else:
+      idx_test_dir = -1
+
+  idx_test_dir = comm.bcast(idx_test_dir,root=0)
+
+  # If the attempted transition is going out of limits,
+  # reject
+  if ((old_idx_hist == 0 and idx_test_dir == -1) or \
+      (old_idx_hist == EEHist.NBins-1 and  idx_test_dir == 1)):
+      WLHist.penalize(old_idx_hist)  
+
+      # update the TMMC collection matrix
+      trans_prob = 0.0 
+      TMHist.update_collection_matrix(old_idx_hist, idx_test_dir, trans_prob)
+  else:
+      # Make the temporary changes to the charge values
+      # Note that these changes have to be reverted if the
+      # attempted transition gets rejected
+      delta_q      = idx_test_dir * delta_q_test
+      q_test_Na    = q_current + delta_q
+      q_test_Cl    = -q_current - delta_q
+      new_idx_hist = EEHist.idx_of(q_test_Na)
+
+      comm.Barrier()
+      flag = lmp.set_variable("q_test_Na", q_test_Na)
+      flag = lmp.set_variable("q_test_Cl", q_test_Cl)
+      lmp.command("set atom ${idx_test_Na} charge ${q_test_Na}")
+      lmp.command("set atom ${idx_test_Cl} charge ${q_test_Cl}")
+
+      # Get the energy by doing a false run
+      lmp.command("run 0")
+      e_new = lmp.extract_compute("thermo_pe",0,0)
+
+      # compute the trans_probability
+      delta_e    = (e_new - e_old)
+      expDE      = np.exp(-Beta * delta_e)
+      trans_prob = np.minimum(expDE,1.0)
+
+      # update the collection matrix of TMMC
+      TMHist.update_collection_matrix(old_idx_hist, idx_test_dir, trans_prob)
+
+      # Processor that has rank zero decides to either
+      # accept or reject the transition and broadcasts
+      # the decision to others
+      if (comm.Get_rank() == 0):
+        delta_w     = WLHist(new_idx_hist) - WLHist(old_idx_hist)
+        arg         = np.exp(-Beta * delta_e + delta_w)
+        acceptTrans = (arg > np.random.rand())
+
+      acceptTrans = comm.bcast(acceptTrans,root=0)
+
+      if (acceptTrans):
+        WLHist.penalize(new_idx_hist)
+        q_current = q_test_Na
+      else:
+        WLHist.penalize(old_idx_hist)
+        q_test_Na = q_current
+        q_test_Cl = -q_current
+        comm.Barrier()
+        flag = lmp.set_variable("q_test_Na", q_test_Na)
+        flag = lmp.set_variable("q_test_Cl", q_test_Cl)
+        lmp.command("set atom ${idx_test_Na} charge ${q_test_Na}")
+        lmp.command("set atom ${idx_test_Cl} charge ${q_test_Cl}")
+        # Need this false run to update the forces correctly
+        lmp.command("run 0")
+
+WL_FILE_out.close()
+
--- tmmc.py
+++ tmmc.py
@@ -1,71 +1,127 @@
 import numpy as np
 import sys
+
+from Hist   import Histogram  # parent class
+from typing import Dict       # used to enforce the parent class typing
+
 # -------------------------------------------------------------
 # A class to support TMMC simulation runs. A transition matrix
 # is stored and can be used to estimate the free energy landscale
 # implied by the subensembles. 
 # -------------------------------------------------------------
-class TMMC_histogram:
+class TMMC_histogram(Histogram):
+  # ----------------------------------
+  # Initialize the class with a histogram of size NSubs
+  def __init__(self,EEHisto: Histogram):
+    self.wts            = EEHisto.binValue
+    NSubs               = EEHisto.NBins
+    self.NSubs          = NSubs
+    self.NVisits        = np.zeros(NSubs,np.int64)
+    self.CM             = np.zeros((NSubs  ,3),np.double) # collection matrix
+    self.TM             = np.ones( (NSubs  ,2),np.double) # transition matrix
+    self.activated      = False                           # if True the simulation uses the wts of TMMC to roam the sub-ensembles
+    self.allSubsLogged  = False                           # if True it is safe to reduce the CM to weights
+
+    # inherited histogram variables
+    self.min            = EEHisto.min
+    self.max            = EEHisto.max
+    self.width_bin      = EEHisto.width_bin
+
+  # ----------------------------------
+  # self-calling the class will return the weight of the requested sub-ensemble i_sub
+  def __call__(self,i_sub):
+    return self.wts[i_sub]
+
+  # ----------------------------------
+  # returns True if trans probabalities for all the sub-ensembles have been logged
+  def check_if_all_subensembles_have_been_logged(self):
+    allSubsLogged = True # assume this is true and check if false
+    for i_sub in range(self.NSubs):
+      if (np.sum(self.CM[i_sub,:]) == 0.0):
+        # in this case no trans_prob has been logged for i_sub yet
+        allSubsLogged = False
+        break
+    return allSubsLogged
 
 
-      # Initialize just with the number of sub-ensembles in the simulation
-      def __init__(self,n_sub):
-          self.n_sub     = n_sub
-          self.visits    = np.zeros(n_sub,np.int64)
-          self.wts       = np.zeros(n_sub,np.double)
-          self.CM        = np.zeros((n_sub  ,3),np.double) # collection matrix
-          self.TM        = np.ones( (n_sub-1,2),np.double) # transition matrix
+  # ----------------------------------
+  # the transition probability between two neighboring sub-ensembles 
+  # from i_sub to a neighbor is stored in the collection matrix. Instead
+  # of giving as input the second sub-ensemble, a direction iDir is
+  # given instead. Applicable values are iDir=-1,0,+1. -1 means a decrease
+  # of the sub-ensemble coordinate, +1 an increase and 0 a move that
+  # keeps us in sub-ensemble i_sub
+  def update_collection_matrix(self,i_sub,iDir,trans_prob):
+    if (iDir < -1 or iDir > 1):
+      sys.exit('update_collection_matrix : ERROR - Invalid direction')
+      
+    i_sub_local = 1
+    j_sub_local = i_sub_local + iDir
+    
+    self.CM[i_sub,j_sub_local] +=  trans_prob 
+    self.CM[i_sub,i_sub_local] += -trans_prob + 1.0 
+
+  # ----------------------------------
+  # use this to compute a weight/free energy and transition matrix
+  # estimate based on the current collection matrix. 
+  def update_TMMC_weights(self):
+    # first avoid doing anything if 
+
+    if (not self.allSubsLogged):
+      self.allSubsLogged = self.check_if_all_subensembles_have_been_logged()
+      if (not self.allSubsLogged):
+        return # leaves the current estimate to be all zeros
+
+    self.wts[0] = 0.0  # set i_sub=0 as the reference state
+
+    # loop over all entries of the transition matrix
+    for i_sub in range(self.NSubs-1):
+      self.TM[i_sub,1]  =  self.CM[i_sub+1,0] / np.sum(self.CM[i_sub+1,:]) # trans i_sub <- i_sub+1
+      self.TM[i_sub,0]  =  self.CM[i_sub  ,2] / np.sum(self.CM[i_sub  ,:]) # trans i_sub -> i_sub+1
+
+      # compute the weight of i_sub+1
+      self.wts[i_sub+1] =  self.wts[i_sub] + np.log( self.TM[i_sub,1] / self.TM[i_sub,0] )
 
 
-      # the transition probability between two neighboring sub-ensembles 
-      # from i_sub to a neighbor is stored in the collection matrix. Instead
-      # of giving as input the second sub-ensemble, a direction iDir is
-      # given instead. Applicable values are iDir=-1,0,+1. -1 means a decrease
-      # of the sub-ensemble coordinate, +1 an increase and 0 a move that
-      # keeps us in sub-ensemble i_sub
-      def update_collection_matrix(self,i_sub,iDir,trans_prob):
-          if (iDir < -1 or iDir > 1):
-            sys.exit('update_collection_matrix : ERROR - Invalid direction')
-            
-          i_sub_local = 1
-          j_sub_local = i_sub_local + iDir
-          
-          self.CM[i_sub,j_sub_local] +=  trans_prob 
-          self.CM[i_sub,i_sub_local] += -trans_prob + 1.0 
+  # ----------------------------------
+  def incr_visits(self,i_sub):
+    self.NVisits[i_sub] += 1
+  
+  # ----------------------------------
+  def reset_visits(self):
+    self.NVisits[:]      = 0
 
+  # ----------------------------------
+  # write the TMMC histogram in a file
+  def write(self,tag,file):
+    dev_from_mean      = np.zeros(self.NSubs) # initialize the deviation from mean visits column
 
+    if (self.activated):
+      mean_visits      = np.mean(self.NVisits)
+      dev_from_mean[:] = self.NVisits[:] / mean_visits # compute deviation from mean visits
+      TMMCInfoString   = "The weights of TMMC are used for roaming the sub-ensembles"
+    else:
+      TMMCInfoString   = "The weights of TMMC are inactive"
 
+    file.write("# %s\n" %tag           )
+    file.write('# %s\n' %TMMCInfoString)
+    file.write("#\n")
 
-      # use this to compute a weight/free energy and transition matrix
-      # estimate based on the current collection matrix. 
-      def update_TMMC_weights(self):
-
-        self.wts    = np.zeros(n_sub,np.double) # re-initialize
-        self.wts[0] = 0.0                       # set i_sub=0 as the reference state
-
-
-        # loop over all entries of the transition matrix
-        for i_sub in range(self.n_sub-1):
-          self.TM[i_sub,0]  =  self.CM[i_sub  ,2] / np.sum(self.CM[i_sub  ,:]) # trans i_sub -> i_sub+1
-          self.TM[i_sub,1]  =  self.CM[i_sub+1,0] / np.sum(self.CM[i_sub+1,:]) # trans i_sub <- i_sub+1
-
-          # compute the weight of i_sub+1
-          self.wts[i_sub+1] =  self.wts[i_sub] + numpy.log( self.TM[i_sub,0] / self.TM[i_sub,1] )
-
-
-
-
-      # ----------------------------------
-      def incr_visits(self,i_sub):
-          self.visits[i_sub] += 1
-
-
-
-      # output a weight estimate in file_out
-      def write_to_a_file(self,file_out):
-          file_out.write("\n")
-          for i_sub in range(self.n_sub):
-             file_out.write('%6.5f %6.5f %6.5f \n' %(i_sub*0.1, self.visits[i_sub]/mean_visits, \
-                       self.wts[i_sub]))
-          file_out.write("\n")
-          file_out.flush()
+    if (not self.allSubsLogged):
+      file.write("# Insufficient sampling - Weights cannot be reduced from the CM\n")
+      file.write("# > Printing the CM instead ...\n")
+      file.write("#  iSub   dir_left     stay     dir_right\n")
+      file.write("#\n")
+      for i_sub in range(self.NSubs):
+        file.write('%6d %10.3f %10.3f %10.3f \n' \
+                 %(i_sub, self.CM[i_sub,0], self.CM[i_sub,1], self.CM[i_sub,2] ))   
+    else:
+      file.write("#   subEnsCoord   devFromMeanVisits      DOS\n")
+      file.write("#\n")
+      for i_sub in range(self.NSubs):
+        subEnsCoord = self.min + i_sub*self.width_bin
+        file.write('%12.3f %16.3f %16.5f \n' \
+                 %(subEnsCoord, dev_from_mean[i_sub], self(i_sub)))
+    
+    file.write("\n")
+    file.flush()
