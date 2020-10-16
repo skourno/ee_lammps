2f93dab0cd02e95b875647cb97303e37079f2fb4
Spiros Kournopoulos <sk1916@ce-amparo05.ce.ic.ac.uk>
2020-10-16 16:50:48 +0100

    all coords are seaved unfolded

--- atom.py
+++ atom.py
@@ -11,20 +11,22 @@ class atom:
 	Type   = -1
 	xyz    = []
 	charge = 0.0
+	Img    = np.zeros(0,np.integer)
 
-	def __init__(self, idx, iSpc, Type, charge, xyz):
+	def __init__(self, idx, iSpc, Type, charge, xyz, Img=np.zeros(0,np.integer)):
 		self.idx    = idx
 		self.iSpc   = iSpc
 		self.Type   = Type
 		self.charge = charge
 		self.xyz    = np.copy(xyz)
+		self.Img    = np.copy(Img)
 
 
 	def change_coords(self, xyz_new, box: domain):
-		self.xyz   = np.copy(xyz_new)
-		self.xyz   = box.fold(self.xyz)
+		self.xyz            = np.copy(xyz_new)
+		#self.xyz, self.Img  = box.fold(self.xyz)
 
 
 	def displace(self, displace, box: domain):
-		self.xyz = list( map(add, self.xyz, displace) )
-		self.xyz = box.fold(self.xyz)
+		self.xyz            = list( map(add, self.xyz, displace) )
+		#self.xyz, self.Img  = box.fold(self.xyz)
