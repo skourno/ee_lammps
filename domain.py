de0001cec97f8eb023c6e386a3e73a3ef30bee7e
Spiros Kournopoulos <sk1916@ce-amparo05.ce.ic.ac.uk>
2020-10-16 16:55:11 +0100

    corrected a folding bug, added an unfolding fun

--- domain.py
+++ domain.py
@@ -1,4 +1,5 @@
 import numpy as np
+import sys
 
 class domain:
 	"""Only rectangular simulation boxes allowed"""
@@ -8,7 +9,20 @@ class domain:
 		self.edge = np.copy(edge)
 
 	def fold(self, xyz):
-		R = np.copy(xyz)
-		R = np.where(R > self.edge, R - self.edge *   np.floor(R/self.edge)     , R ) 
-		R = np.where(R < 0.0      , R + self.edge * (-np.floor(R/self.edge) + 1), R )
-		return R
+		Img = np.zeros(0,np.integer) 
+		R   = np.copy(xyz)
+
+		R_gt_edge = np.array([r > e   for r,e in zip(R,self.edge)] )
+		R_lt_zero = np.array([r < 0.0 for r   in R] )
+
+		Img = np.floor(R/self.edge)
+		Img = np.array([ int(i) for i in Img ])
+
+		R   = np.where(R_gt_edge, R - self.edge *   np.floor(R/self.edge), R )
+		R   = np.where(R_lt_zero, R + self.edge *  -np.floor(R/self.edge), R )
+
+		return R, Img
+
+	def unfold(self, xyz, Img):
+		return np.array([r + e*i for r,e,i in zip(xyz, self.edge, Img)])
+
